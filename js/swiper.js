9145f8b9cb92942e1e00f2f4f0ba721feec4e5e8
adityagofi <gofisaputra4@gmail.com>
2024-02-02 04:10:24 +0700

    Initial commit

---
+++ js/swiper.js
@@ -0,0 +1,15 @@
+
+  var swiper = new Swiper('.swiper-container', {
+    autoplay: {
+      delay: 5000,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    // Add more options as needed
+  });
